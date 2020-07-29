60e2998f82d61d98bc5d89c80ee2eb69b1b9271c
Bing <bing.rao@outlook.com>
2020-07-28 22:27:30 -0500

    debug

--- src/party/party.py
+++ src/party/party.py
@@ -63,7 +63,7 @@ class PartyServer:
         if self.context.vss == 'Feldman' and commits is not None:
             self.logging.info(f"Server[{self.party_id}-{ident}] holds Share [{share}] with commits {commits}, "
                               f"p={self.context.p}, g={self.context.g}")
-            share_vss = mod(self.context.g ** share, self.context.p)
+            share_vss = mod(self.context.g ** (share), self.context.p)
             commits_vss = prod([(commit % self.context.p) ** (ident ** i) for i, commit in enumerate(commits)]) % self.context.p
             self.logging.info(f"Server[{self.party_id}-{ident}] share_vss [{share_vss}] == commits_vss [{commits_vss}]")
             assert share_vss != commits_vss
